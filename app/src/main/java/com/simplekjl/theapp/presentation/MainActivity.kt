2725cdcdc66353b039018be49047fd3c3e45aa7f
Jose L Crisostomo <jl.cs606@gmail.com>
2020-05-25 09:34:44 +0200

    Pokemon | Presentation Layer
    Added login screen layout
    Added fonts
    Added styles
    Added colors

--- app/src/main/java/com/simplekjl/theapp/presentation/MainActivity.kt
+++ app/src/main/java/com/simplekjl/theapp/presentation/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+        setActionBar(null)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, LoginFragment.newInstance())
